ba9d051fa29f69efe23c50fc75258aeeee2fcbbe
CYF <996174599@qq.com>
2020-03-20 19:03:10 +0800

    完成“关注、取关”功能开发

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -6,6 +6,8 @@ public class RedisKeyUtil {
     private static final String PREFIX_ENTITY_LIKE="like:entity";
     private static final String PREFIX_USER_LIKE = "like:user";
 
+    private static final String PREFIX_FOLLOWEE = "followee";  //被关注的人
+    private static final String PREFIX_FOLLOWER = "follower";  //关注别人的人
     //某个实体的赞
     //like:entity:entityType:entityId    --> set(userId)
     public static String getEntityLikeKey(int entityType,int entityId){
@@ -18,4 +20,17 @@ public class RedisKeyUtil {
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
     }
+
+    // 某个用户关注的实体
+    // followee:userId:entityType -> zset(entityId,now)
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // 某个实体拥有的粉丝
+    // follower:entityType:entityId -> zset(userId,now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
+    }
+
 }
