448957deeb340e01995e9829818f4b95fc9cd740
CYF <996174599@qq.com>
2020-03-22 22:30:20 +0800

    完成“搜索”功能

--- src/main/java/com/nowcoder/community/entity/DiscussPost.java
+++ src/main/java/com/nowcoder/community/entity/DiscussPost.java
@@ -1,20 +1,43 @@
 package com.nowcoder.community.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
 import java.util.Date;
 
 /**
  * 帖子的实体类
  */
+@Document(indexName = "discusspost", type = "_doc", shards = 6, replicas = 3)
 public class DiscussPost {
 
+    @Id
     private int id;       //帖子id
+
+    @Field(type = FieldType.Integer)
     private int userId;   //用户id
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String title; //帖子标题
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String content; //帖子内容
+
+    @Field(type = FieldType.Integer)
     private int type;      //帖子类型 0-普通  1-置顶
+
+    @Field(type = FieldType.Integer)
     private int status;   //帖子状态 0-正常 1-精华 2-拉黑
+
+    @Field(type = FieldType.Date)
     private Date createTime;  //帖子的创建日期
+
+    @Field(type = FieldType.Integer)
     private int commentCount;  //帖子的评论数量
+
+    @Field(type = FieldType.Double)
     private double score; //帖子的分数
 
     public int getId() {
